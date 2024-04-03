c83ddb3673fcd53a24fb12e8d36ae43d936ba7d9
Austin Tan Jun Heng <austintjh19@gmail.com>
2024-04-03 16:31:47 +0800

    Setup Query Form

--- TravelSuggest/base/views.py
+++ TravelSuggest/base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import QueryData, ResultData
+from .forms import QueryForm
 
 def home(request):
+    queryForm = QueryForm()
     queryData = QueryData.objects.last()
     resultsData = ResultData.objects.filter(query=queryData)
-    data = {'QueryData': queryData,'ResultsData': resultsData}
+    data = {'QueryForm': queryForm, 'QueryData': queryData,'ResultsData': resultsData}
     return render(request, 'base/home.html', data)
