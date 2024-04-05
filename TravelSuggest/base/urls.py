c27ee356c12e87247ea8e610b7ebef12086f1195
Austin Tan Jun Heng <austintjh19@gmail.com>
2024-04-05 23:14:19 +0800

    Enhance UI

--- TravelSuggest/base/urls.py
+++ TravelSuggest/base/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('query-result/', views.queryResult, name="query-result"),
 ]
--- TravelSuggest/base/views.py
+++ TravelSuggest/base/views.py
@@ -3,22 +3,18 @@ from .models import QueryData, ResultData
 from .forms import QueryForm
 
 def home(request):
-    queryForm = QueryForm()
-
-    if request.method == 'POST':
-        form = QueryForm(request.POST)
-        if form.is_valid():
-            print(request.POST)
-            form.save()
-            return redirect('query-result')
-
-    data = {'QueryForm': queryForm}
-
-    return render(request, 'base/home.html', data)
-
-def queryResult(request):
     queryData = QueryData.objects.last()
     resultsData = ResultData.objects.filter(query=queryData)
 
-    data = {'QueryData': queryData,'ResultsData': resultsData}
-    return render(request, 'base/queryresult.html', data)
+    queryForm = QueryForm(instance=queryData)
+
+    if request.method == 'POST':
+        form = QueryForm(request.POST)                          # form = QueryForm(request.POST, instance=queryData) to repplace existing query
+        if form.is_valid():
+            form.save()
+
+    data = {'QueryForm': queryForm, 
+            'QueryData': queryData,
+            'ResultsData': resultsData}
+
+    return render(request, 'base/home.html', data)
